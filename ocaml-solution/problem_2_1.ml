4c59f7697ce36f06a261aab4404e1172be1f4165
jkulrativid <jetnipit.kul@blockfint.com>
2025-12-26 21:41:44 +0700

    save progress before migrating to dune package manager

---
+++ ocaml-solution/problem_2_1.ml
@@ -0,0 +1,60 @@
+(*let file_to_run = "problem_2_test.txt"*)
+let file_to_run = "problem_2_input.txt"
+
+(* util funcs *)
+let string_list_to_string l = List.filter (fun s -> s <> "") l |> String.concat "\n"
+
+let read_lines_modern (filename : string) =
+  let ic = open_in filename in
+  let rec loop prev : string list =
+    match In_channel.input_line ic with
+    | Some line -> line :: loop ()
+    | None -> []
+  in
+  loop ()
+;;
+
+let str_inc s = int_of_string s |> ( + ) 1 |> string_of_int
+(*end util funcs*)
+
+(*  TODO: sort and merge ID range before validating each reduces redundant operation, *)
+(* but in the FPGA world I'm not sure, needing verification. *)
+
+let is_valid_id (id : string) : bool =
+  let len = String.length id in
+  match len mod 2 with
+  | 1 -> true
+  | otherwise ->
+    let half_len = len / 2 in
+    let first_half = String.sub id 0 half_len in
+    let second_half = String.sub id half_len half_len in
+    first_half <> second_half
+;;
+
+let get_sum_invalid_ids_from_id_range (start : string) (_end : string) : int =
+  let stop = str_inc _end in
+  let rec iterate (current_id : string) : int =
+    if current_id = stop then
+      0
+    else if is_valid_id current_id then
+      iterate (str_inc current_id)
+    else
+      int_of_string current_id + iterate (str_inc current_id)
+  in
+  iterate start
+;;
+
+let get_sum_of_invalid_ids_from_id_ranges (ranges : (string * string) list) : string =
+  List.fold_left
+    (fun sum (start, _end) -> get_sum_invalid_ids_from_id_range start _end + sum)
+    0
+    ranges
+  |> string_of_int
+;;
+
+read_lines_modern file_to_run
+|> List.hd
+|> String.split_on_char ','
+|> List.map (fun raw_str -> Scanf.sscanf raw_str "%[^-]-%s" (fun a b -> a, b))
+|> get_sum_of_invalid_ids_from_id_ranges
+|> print_endline
